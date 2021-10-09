b8b0b8263ce0edc4408cecd8381c5ae6f46a6f8a
Leslieghf <lesliejohnrichardson@gmail.com>
2021-10-09 21:16:24 +0200

    Added files

---
+++ forum/data.js
@@ -0,0 +1,40 @@
+var threads = [
+    {
+        id: 0,
+        title: "Thread 0",
+        author: "Dennis H.",
+        date: Date.now(),
+        content: "Thread content",
+        comments: [
+            {
+                author: "Peter",
+                date: Date.now(),
+                content: "Hey there"
+            },
+            {
+                author: "Dennis B.",
+                date: Date.now(),
+                content: "Hey to you too"
+            }
+        ]
+    },
+    {
+        id: 1,
+        title: "Thread 1",
+        author: "Jonathan",
+        date: Date.now(),
+        content: "Thread content Version 2",
+        comments: [
+            {
+                author: "Christian",
+                date: Date.now(),
+                content: "Bye there"
+            },
+            {
+                author: "Sebastian",
+                date: Date.now(),
+                content: "Bye to you too"
+            }
+        ]
+    }
+]
